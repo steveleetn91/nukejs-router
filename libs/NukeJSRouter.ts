15f40f0f50f0758e3204d1f9c6fd89aa069c0158
adminhopee <hoang.le.tn91@gmail.com>
2022-12-31 22:37:00 +0700

    Update core

--- libs/NukeJSRouter.ts
+++ libs/NukeJSRouter.ts
@@ -1,45 +1,68 @@
 interface NukeJSRouterInterface {
-    import(lists : Array<{
-        url : string,
-        page : Promise<any>
-    }>,notFound : NukeJSItemRouter) : void;
+    import(lists: Array<{
+        url: string,
+        page: Promise<any>
+    }>, notFound: NukeJSItemRouter): void;
 }
 interface NukeJSItemRouter {
-    url : string,
-    page : Promise<any>
+    url: string,
+    page: Promise<any>
 }
-export default class NukeJSRouter implements NukeJSRouterInterface{
-    private listRouter : Array<NukeJSItemRouter> | undefined;
-    private notFound : NukeJSItemRouter | undefined;
-    import(lists : Array<NukeJSItemRouter>,notFound : NukeJSItemRouter): void {
+export default class NukeJSRouter implements NukeJSRouterInterface {
+    private listRouter: Array<NukeJSItemRouter> = [];
+    private notFound: NukeJSItemRouter | undefined;
+    import(lists: Array<NukeJSItemRouter>, notFound: NukeJSItemRouter): void {
         this.listRouter = lists;
         this.notFound = notFound;
         this.walk();
     }
-    private walk(oldUrl : string = ""): void {
-        this.listRouter?.forEach((item,index) => {
-            const currentlyItem = item.url;
-            if(window.location.pathname === oldUrl) {
-                return;
-            }
-            oldUrl = window.location.pathname;
-            if(currentlyItem === window.location.pathname) {
-                item.page.then( async (page) => {
-                    let _window : any = window;
-                    _window.nukepage = new page.default();
-                    await _window.nukepage.beforeRender();
-                    await _window.nukepage.render();
-                    await _window.nukepage.afterRender();
-                    try {
-                        window = _window;
-                    }catch(e) {
-                        //ignore warning 
-                    }
-                });
-            }
-        });
-        setTimeout( async () => {
+    private walk(oldUrl: string = ""): void {
+        if (window.location.pathname !== oldUrl) {
+            let notFound = true;
+            this.listRouter.forEach(async (item, index) => {
+
+                if (item.url === window.location.pathname) {
+                    notFound = false;
+                    console.log("check", item.url);
+                    item.page.then(async (page) => {
+                        let _window: any = window;
+                        if (_window.nukepage) {
+                            delete _window.nukepage;
+                        }
+                        _window.nukepage = new page.default();
+                        await _window.nukepage.beforeRender();
+                        await _window.nukepage.render();
+                        await _window.nukepage.afterRender();
+                        try {
+                            window = _window;
+                        } catch (e) {
+                            //ignore warning 
+                        }
+                    });
+                }
+                if (index === (this.listRouter.length - 1) && notFound === true) {
+                    this.notFound?.page.then(async (page) => {
+                        let _window: any = window;
+                        if (_window.nukepage) {
+                            delete _window.nukepage;
+                        }
+                        _window.nukepage = new page.default();
+                        await _window.nukepage.beforeRender();
+                        await _window.nukepage.render();
+                        await _window.nukepage.afterRender();
+                        try {
+                            window = _window;
+                        } catch (e) {
+                            //ignore warning 
+                        }
+                    })
+                }
+            });
+        }
+        oldUrl = window.location.pathname;
+        setTimeout(async () => {
+            //console.log("check",window.location.pathname);
             await this.walk(oldUrl);
-        },300);
+        }, 300);
     }
 }
